82fb480887dc9478c57cbc048005f6cbd0e8c755
RishatTalukder <120198784+RishatTalukder@users.noreply.github.com>
2023-06-15 21:49:15 +0600

    sdfvf

--- server.py
+++ server.py
@@ -3,61 +3,63 @@ from grid import Grid
 import os
 
 
-#threading
+# threading
 import threading
+
 
 def create_thread(target):
     thread = threading.Thread(target=target)
-    thread.daemon = True #this makes is so that these thread will auto quit before the code ends running
+    thread.daemon = True  # this makes is so that these thread will auto quit before the code ends running
     thread.start()
 
-#socket
+
+# socket
 import socket
 
 HOST = "127.0.0.1"
 PORT = 65432
 
-ADDR = (HOST,PORT)
+ADDR = (HOST, PORT)
 connection_established = False
-conn , addr = None, None
+conn, addr = None, None
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     sock.bind(ADDR)
 except socket.error as e:
     print(str(e))
-    
+
 sock.listen(1)
+
 
 def receive_data():
     global turn, grid, player
     while True:
         data = conn.recv(1024).decode()
         data = data.split("-")
-        x , y = int(data[0]) , int(data[1])
+        x, y = int(data[0]), int(data[1])
         if data[2] == "yourturn":
             turn = True
-        if data[3]  == "False":
+        if data[3] == "False":
             if data[4] == "None":
                 grid.winner = None
             else:
                 grid.winner = data[4]
-            grid.game_over = True 
+            grid.game_over = True
         if grid.get_cell_value(x, y) == 0:
             grid.set_cell_value(x, y, "O")
 
         print(data)
 
 
-
-os.environ['SDL_VIDEO_WINDOW_POS'] = "500, 150"
+os.environ["SDL_VIDEO_WINDOW_POS"] = "500, 150"
 
 width = 600
 height = 600
 
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tic-tac-toe")
-programIcon = pygame.image.load('images/icon_tic_tac_toe.png')
+programIcon = pygame.image.load("images/icon_tic_tac_toe.png")
 pygame.display.set_icon(programIcon)
 
 # set variables
@@ -66,7 +68,7 @@ player = "X"
 turn = True
 playing = "True"
 grid = Grid()  # making the grid object
-clock = pygame.time.Clock() 
+clock = pygame.time.Clock()
 
 
 def waiting_for_connection():
@@ -74,8 +76,11 @@ def waiting_for_connection():
 
     print("Waiting for connection....")
     grid.waiting_for_conn = True
-    
-    conn , addr = sock.accept() # it will wait for a connection , also blocks any new threads 
+
+    (
+        conn,
+        addr,
+    ) = sock.accept()  # it will wait for a connection , also blocks any new threads
     print("Client is connected!!!")
 
     grid.waiting_for_conn = False
@@ -83,43 +88,52 @@ def waiting_for_connection():
 
     receive_data()
 
+
 create_thread(waiting_for_connection)
 
-def main() :
+
+def main():
     global running, turn, grid, player, playing, clock
 
-    while running: 
-        clock.tick(60) # frames i think
+    while running:
+        clock.tick(60)  # frames i think
 
-        if turn and not(grid.game_over):
+        if turn and not (grid.game_over):
             grid.waiting_for_move = False
-        elif ((not turn) and not(grid.game_over)):
+        elif (not turn) and not (grid.game_over):
             grid.waiting_for_move = True
 
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN and (not grid.game_over) and connection_established:
+            if (
+                event.type == pygame.MOUSEBUTTONDOWN
+                and (not grid.game_over)
+                and connection_established
+            ):
                 if pygame.mouse.get_pressed()[0]:
-                    if turn and not(grid.game_over):
-
+                    if turn and not (grid.game_over):
                         pos = pygame.mouse.get_pos()
-                        Cell_X, Cell_Y = pos[0]//200, pos[1]//200 #this gets me the cell being clicked
+                        Cell_X, Cell_Y = (
+                            pos[0] // 200,
+                            pos[1] // 200,
+                        )  # this gets me the cell being clicked
 
-                        if grid.get_cell_value(Cell_X,Cell_Y) == 0:
+                        if grid.get_cell_value(Cell_X, Cell_Y) == 0:
                             grid.get_mouse(Cell_X, Cell_Y, player)
                             if grid.game_over:
-                                playing  = "False"
-                            #server sending data
-                            send_data = "{}-{}-{}-{}-{}".format(Cell_X, Cell_Y, 'yourturn', playing, str(grid.winner)).encode()
+                                playing = "False"
+                            # server sending data
+                            send_data = "{}-{}-{}-{}-{}".format(
+                                Cell_X, Cell_Y, "yourturn", playing, str(grid.winner)
+                            ).encode()
                             conn.send(send_data)
 
                             turn = False
 
                     # grid.print_grid()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and grid.game_over:                     
+                if event.key == pygame.K_SPACE and grid.game_over:
                     grid.clear_grid()
                     player = "X"
                     grid.game_over = False
@@ -128,11 +142,11 @@ def main() :
                 elif event.key == pygame.K_ESCAPE:
                     running = False
 
-
         # rgba(116, 185, 255,1.0)
         # (22,32,101)
         grid.draw(win)
 
         pygame.display.flip()
+
 
 main()
